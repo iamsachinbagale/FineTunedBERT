10bfeb0ffcc91a5e568e4ab54c43c0dc7ea05dcd
sachinbagale <sachinworld1234@gmail.com>
2024-06-15 01:08:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,38 @@ import streamlit as st
 import numpy as np
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
+import requests
+import base64
+import matplotlib.pyplot as plt
+
+def set_background(image_file):
+    """
+    This function sets the background of a Streamlit app to an image specified by the given image file.
+
+    Parameters:
+        image_file (str): The path to the image file to be used as the background.
+
+    Returns:
+        None
+    """
+    with open(image_file, "rb") as f:
+        img_data = f.read()
+    b64_encoded = base64.b64encode(img_data).decode()
+    style = f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{b64_encoded});
+            background-size: cover;
+        }}
+        </style>
+    """
+    st.markdown(style, unsafe_allow_html=True)
+
+set_background('Image.jpg')
+
+plt.style.use("ggplot")
+
+st.title("Toxic Comment Classification")
 
 @st.cache(allow_output_mutation=True)
 def get_model():
